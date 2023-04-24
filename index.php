44b64be7cdafefb7b43a848156b94f80940c3b7e
Kondauciel <luis.molina08@unach.mx>
2023-04-24 00:51:16 -0600

    mmmmm

--- index.php
+++ index.php
@@ -9,11 +9,7 @@
 <body>
     <h1>Lista de productos</h1>
     <div>
-<<<<<<< HEAD
-        <a href="nuevoproducto.php">Nuevo</a>
-=======
         <a href="nuevoProducto.php">Nuevo</a>
->>>>>>> b2883d1ece7a5489d7899625a046b0c608b37c54
         <table>
             <thead>
                 <tr>
@@ -32,17 +28,10 @@
                     if(count($resultado)>0){
                         foreach ($resultado as $registro) {
                         echo '<tr>';
-<<<<<<< HEAD
-                        echo '<th>'.$registro['Id'].'</th>';
-                        echo '<th>'.$registro['Nombre'].'</th>';
-                        echo '<th>'.$registro['Precio'].'</th>';
-                        echo '<th>'.$registro['Existencias'].'</th>';
-=======
                         echo '<td>'.$registro['Id'].'</td>';
                         echo '<td>'.$registro['Nombre'].'</td>';
                         echo '<td>'.$registro['Precio'].'</td>';
                         echo '<td>'.$registro['Existencias'].'</td>';
->>>>>>> b2883d1ece7a5489d7899625a046b0c608b37c54
                         echo '</tr>';
                         }
                     }
