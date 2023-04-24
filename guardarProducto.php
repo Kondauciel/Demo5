b47f417eedff61deb3c8aef2c3a8bec89e15c602
Kondauciel <luis.molina08@unach.mx>
2023-04-24 00:51:01 -0600

    guardar

--- guardarProducto.php
+++ guardarProducto.php
@@ -2,35 +2,18 @@
 $nombre=$_POST['nombre'];
 $precio=$_POST['precio'];
 $existencias=$_POST['existencias'];
-<<<<<<< HEAD
-if(!empty($nombre)&&!empty($precio)&&!empty($existencias)){
+if (!empty($nombre)&&!empty($precio)&&!empty($existencias)){
     require_once'Models/Producto.php';
     $producto=new Producto();
     $resultado=$producto->InsertarProducto($nombre,$precio,$existencias);
-=======
-if(!empty($nombre)&& !empty($precio)&& !empty($existencias)){
-    require_once 'Models/Producto.php';
-    $producto = new Producto();
-    $resultado=$producto->InsertarProducto($nombre,
-    $precio,$existencias);
->>>>>>> b2883d1ece7a5489d7899625a046b0c608b37c54
-    if($resultado){
+    if ($resultado){
         header("Location:index.php");
     }
-    else{
-<<<<<<< HEAD
-        header("Location:nuevoProducto.php"); 
-=======
-        header("Location:nuevoProducto.php");
->>>>>>> b2883d1ece7a5489d7899625a046b0c608b37c54
+    else {
+        header ("Location:nuevoProducto.php"); 
     }
 }
-else{
+else {
     header("Location:nuevoProducto.php");
 }
-<<<<<<< HEAD
-
 ?>
-=======
-?>
->>>>>>> b2883d1ece7a5489d7899625a046b0c608b37c54
